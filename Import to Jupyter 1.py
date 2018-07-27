d20e3fd228da2d56b3c1539bb27d5c3460cd04bd
Brandon Marco <36774208+bmarco1@users.noreply.github.com>
2018-07-27 13:57:03 -0400

    Add files via upload

--- Import to Jupyter 1.py
+++ Import to Jupyter 1.py
@@ -1,3 +1,6 @@
+
+#Fetching the Data
+#Creates a dataset directory in the workspace
 import os
 import tarfile
 from six.moves import urllib
@@ -14,28 +17,45 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
+#Break---
+
+
+#Loading the Data
+#Returns a Pandas dataframe object
+
 import pandas
 def load_housing_data(hosuing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
+
+#Break---
+
+#Histogram plot
+#Shows the number of instances that have a given range
 
 %matplot inline
 import matplotlib.pyplot as plt
 housing.hist(bins=50, figsize=(20,15))
 plt.show()
 
+#Break---
+
+#Creating a test set
+#Typically set aside 20% of the dataset
+
 import numpy as np
 def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
+    shuffled_indices = np.random.permutation(len(data)) #random number generator
     test_set_size = int(len(data) * test_ratio)
     test_indices = shuffled_indices[:test_set_size]
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
-
+#Use the function
 >>>train_set, test_set = split_train_test(housing, 0.2)
 >>>print (len(train_set), "train +", len(test_set), "test")
 16512 train + 4128 test
 
+#hash table, save the test set on the first run to be used in subsequent Runs
 
 import hashlib
 
@@ -46,16 +66,28 @@ def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
     ids = data[id_column]
     in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
     return data.loc[~in_test_set], data.loc[in_test_set]
-
+#Add an Index columsn, this will act as a unique identifier
 housing_with_id =housing.reset_index()
 train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
-
+#May also use a stable feature as the unique identifier, such as longitude/latitude
 housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
 train_set, test_set = split_train_test_by_id(hosuing_with_id, 0.2, "id")
+
+#scikit-learn provides functions to split a dataset
+from sklearn.model_selection import train_split
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+#Break---
+
+#Create an income category attribute
+#This code divides the median income by 1.5 to limit the number of income categories
 
 hosuing["income_cat"] = np.ceil(housing["median_income"]/ 1.5)
 housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
 
+#Break---
+
+#Begin Stratified Sampling
 
 from sklearn.model_selection import StratifiedShuffleSplit
 
@@ -63,57 +95,85 @@ split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_train_set = housing.loc[train_index]
     strat_test_set = housing.loc[test_index]
-
+#Use the function
 >>>strat_test_set["income_cat"].value_counts() / len(strat_test_set)
 
+#Break---
+
+#Because the sample income distribution is identical to the full dataset, we cane remove the income_cat to restore the dataset to it's original state
 
 for set_in (strat_train_set, strat_test_set):
-    set_.drop ("income_cat", axis=1, inslpace=True)
+    set_.drop ("income_cat", axis=1, inplace=True)
 
+#Break---
+
+#Visualixe the Data
+#Create a copy of the training set, so we don't screw it all up :)
 
 housing = strat_train_set.copy()
 
+#Create a geographical scatter plot with long/lat. Use an alpha = 0.1 to make the highly dense population areas more noticeable
+
 housing.plot(kind="scatter", x="longitude", y="latitiude", alpha=0.1)
 
+#Investigate housing prices
 
-housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4, s=housing["population"] / 100, lavel="population", figsize=(10,7), c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,)
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4, s=housing["population"] / 100, label="population", figsize=(10,7), c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,)
 plt.legend()
 
+#Break---
+
+#Correlation Coefficient
 
 corr_matrix = housing.corr()
 
+#Break---
 
+# Another way to check for correlations
 
 from pandas.plotting import scatter_matrix
 
 attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
 scatter_matrix(housing[attributes], figsize=(12, 8))
 
+#Zoom in on the most important correlation from above
+
 housing.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
+
+>>>corr_matrix = housing.corr()
+>>>corr_matrix["median_house_value"].sort_values(ascending=False)
+
+#Break---
+
+#Attribute Combinations
 
 housing["rooms_per_housholds"] = housing["total_rooms"/housing"households"]
 housing["bedrooms_per_room"] = housing["total_bedrooms"/housing"total_rooms"]
 housing["population_per_housholds"] = housing["population"/housing"households"]
 
->>>corr_matrix = housing.corr()
->>>corr_matrix["median_house_value"].sort_values(ascending=False)
-
-
-
-
+#Get back to a clean set of data before Data Cleaning
 
 housing = strat_train_set.drop("median_house_value", axis=1)
 housing_labels = strat_train_set["median_house_value"].copy()
-housing["total_bedrooms"].fillna.(median, inplace=True)
+housing["total_bedrooms"].fillna.(median, inplace=True)    #????????? Out of Place
+
+#Begin Data Cleaning, get rid of corresponding districts, get rid of whole attributes, set values
+    #Option 1: housing.dropna(subset=["total_bedrooms"])
+    #Option 2: housing.drop("total_bedrooms", axis=1)
+    #Option 3: median=housing ["total_bedrooms"].median()
+    # housing["total_bedrooms"].fillna(median, inplace = True)
+
+#Use Sci-scikit to replace missing values in the test set, Option 3
 
 from sklearn.preprocessing import Imputer
-imputer = Imputer(strategy="median")
-housing_num = housing.drop("ocean_proximity", axis=1)
+imputer = Imputer(strategy="median")                      #the imputer computes the median of each attribute, also known as an estimator
+housing_num = housing.drop("ocean_proximity", axis=1)     #We need to drop ocean_proximity since the median can only be computed in numerical attributes
 imputer.fit(housing_num)
 
-X = imputer.transform(housing_num)
-housing_tr = pd.DataFrame(X, colums=housing_num.columns)        #to return to Pandas DataFrame
+X = imputer.transform(housing_num)                        #Use the "trained" imputer to transform the training set. Replacing the missing values
+housing_tr = pd.DataFrame(X, colums=housing_num.columns)        #to return to Pandas DataFrame, Optional
 
+#Break---
 
 #to change ocean_proximity into a numeric value if we didn't use the Imputer
 >>>housing_cat_encoded, housing_categories = housing_cat.factorize()
@@ -386,7 +446,7 @@ class Never5Classifier(BaseEstimator):
     def predict(self, x):
         return np.zeros((len(x), 1), dtype=bool)
 
->>>never_5_clf = Never5Classifier()
+>>>never_5_clf = Never5Classifier()0000
 >>>cross_val_score(never_5_clf, x_train, y_train_5, cv=3, scoring="accuracy")
 
 #Confusion Matrix
@@ -411,3 +471,24 @@ y_train_pred = cross_val_predict(sgd.clf, x_train, y_train_5, cv=3)
 >>>y_scores
 >>>threshold = 0
 >>>y_some_digit_pred = (y_scores > threshold)
+
+
+#ROC Curve
+from sklearn.metrics import roc_curve(fpr, tpr, label=None)
+
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+#FRP againdt the tpr
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0. 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
+plot_roc_curve(fpr, tpr)
+plt.show()
+
+#ROC accuracy
+
+>>>from sklearn.metrics import roc_auc_score
+>>>roc_auc_score(y_train_5, y_scores)
